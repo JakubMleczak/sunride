11182c35776b23e3786b6db16957a7a9fc5963e4
Kuba <jakub.mleczak2000@gmial.com>
2021-04-08 19:15:46 +0100

    blog test

--- blog.php
+++ blog.php
@@ -64,16 +64,19 @@ $title = "Team SunrIde"
 	<script type="text/javascript" src="assets/plugins/form/jquery.form.min.js"></script>
 	<script type="text/javascript" src="assets/plugins/form/jquery.validate.min.js"></script>
 	<script type="text/javascript" src="assets/plugins/light/js/lightbox-plus-jquery.js"></script>
-	<script>
-		// Selecting the iframe element
-		var iframe = document.getElementById("myIframe");
-		alert("test")
-		// Adjusting the iframe height onload event
-		iframe.onload = function() {
-			console.log(iframe.contentWindow.document.body.scrollHeight)
-			iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
-			
-		}
-	 </script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/plugins/iframe/js/iframeResizer.min.js"></script>
+    <script type="text/javascript">
+      /*
+       * If you do not understand what the code below does, then please just use the
+       * following call in your own code.
+       *
+       *   iFrameResize({log:true});
+       *
+       * Once you have it working, set the log option to false.
+       */
+
+	  iFrameResize({ log: true }, '#myIframe')
+    </script>
 
 </body>
