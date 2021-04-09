017d2a80783f1ff72f78fda7acb21790b5b87139
Kuba <jakub.mleczak2000@gmial.com>
2021-04-09 17:31:44 +0100

    exp

---
+++ indexexperimental.php
@@ -0,0 +1,602 @@
+<?php
+
+$title = "Team SunrIde"
+
+?>
+
+<head>
+
+	<?php
+
+	include('phpCore/template.php')
+
+	?>
+
+</head>
+
+<body>
+
+	<div class="site-loader"></div>
+
+	<section id="home" class="site-splash fullscreen">
+
+		<div class="splash-slider active cycle-paused">
+
+
+
+
+
+
+
+			<div class="cycle-next"></div>
+			<div class="cycle-prev"></div>
+			<div class="splash-content cycle-slide cycle-slide-active" style="position: absolute; top: 0px; left: 0px; z-index: 98; visibility: visible; display: block; opacity: 1;">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+
+								<h2 class="splash-headline">Welcome to </h2>
+								<img style="width:250px; margin-top:-40px;" src="./Team SunrIde_files/logoend.png">
+								<!-- Logo is at the bottom of Welcome -->
+								<div class="splash-description">
+									<p>Sheffield University Nova Rocket Innovative Design Engineering </p>
+									<p style="padding-top:20px;">The first student-led
+										<span> Rocket Design Team</span> to represent the UK in the world's largest International University rocket competition
+										<br>
+										<span class="text-primary">22 - 26 June, 2021</span>
+									</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+			<div class="splash-content cycle-slide" style="position: absolute; top: 0px; left: 0px; display: block; opacity: 1; z-index: 97; visibility: hidden;">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+								<h4 class="splash-intro">
+									<a href="http://sunride.group.shef.ac.uk/#">TEAM SUNRIDE</a>
+								</h4>
+								<h1 class="splash-headline">The University of Sheffield </h1>
+								<div class="splash-description">
+									<p>Groundbreaking ideas that will redefine the course of rocket design in the UK</p>
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+			<div class="splash-content cycle-slide" style="position: absolute; top: 0px; left: 0px; display: block; opacity: 1; z-index: 99; visibility: hidden;">
+
+				<div class="container">
+
+					<div class="row">
+
+						<div class="col-md-12">
+
+							<div class="splash-text">
+
+								<h4 class="splash-intro">
+									<a href="http://sunride.group.shef.ac.uk/#">SAC 2021</a>
+								</h4>
+								<img scr="demo/TEST/4.png">
+								<h1 class="splash-headline">SPACEPORT
+									<span class="text-primary"> America</span> 2021
+								</h1>
+								<div class="splash-description">
+									<p>
+										<span class="text-primary">22 - 26 June, 2021</span>
+									</p>
+								</div>
+								<div class="splash-description">
+								</div>
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+
+
+		<div class="splash-media splash-media-img has-bg img-back" >
+			<!-- change the url("") for different background using the full directory -->
+			<div class="overlay"></div>
+		</div>
+		<div class="splash-feedback">
+			<span class="mouse">
+				<span class="fa fa-angle-down"></span>
+			</span>
+
+			<span class="caption">Scroll for More</span>
+		</div>
+
+	</section>
+
+	<?php
+
+	include('phpCore/nav.php')
+
+	?>
+
+
+
+	<div class="site-wrapper">
+
+		<div class="site-body">
+
+			<section id="about" class="section">
+
+				<div class="section-row-container">
+
+					<div class="top-margin bottom-margin">
+
+						<div class="main-gallery js-flickity" data-flickity-options='{"wrapAround": "true", "cellAlign": "left", "contain": true, "autoPlay": "true", "autoPlay": "1500" }'>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/helen.jpg');">
+								<a href="helen.php">
+									<!-- <img src="Pics/helen.jpg" alt="rocket photo"> -->
+									<h1 class="h1-exp">HELEN</h1>
+									<h2 class="h2-exp">Our world breaking achievement</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment"  style="background-image: url('Pics//vesna3.png');">
+								<a href="vesna.php">
+									<!-- <img src="Pics/vesna3.png" alt="rocket photo"> -->
+									<h1 class="h1-exp">VESNA</h1>
+									<h2 class="h2-exp">A highly-engineered mission</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/sunfire.png');">
+								<a href="sunfire.php">
+									<!-- <img src="Pics/sunfire.png" alt="rocket photo"> -->
+									<h1 class="h1-exp">SunFire</h1>
+									<h2 class="h2-exp">Liquid Rocket Engine Sheffield Hallam University</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/karman2.jpg');">
+								<a href="karman.php">
+									<!-- <img src="Pics/karman2.jpg" alt="rocket photo"> -->
+									<h1 class="h1-exp">Karman A.</h1>
+									<h2 class="h2-exp">Students Shot 2 Space</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/jr.png');">
+								<a href="sunride.php">
+									<!-- <img src="Pics/jr.png" alt="rocket photo"> -->
+									<h1 class="h1-exp">SunrIde Jr</h1>
+									<h2 class="h2-exp">Our Society Project</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/EARL1.jpg');">
+								<a href="space.php">
+									<!-- <img src="Pics/EARL1.jpg" alt="rocket photo"> -->
+									<h1 class="h1-exp">Space Dart</h1>
+									<h2 class="h2-exp">Spacefleet Ltd Partnership</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+							<div class="gallery-cell gallery-experiment" style="background-image: url('Pics/amy.jpg');">
+								<a href="amy.php">
+									<!-- <img src="Pics/amy.jpg" alt="rocket photo"> -->
+									<h1 class="h1-exp">AMY</h1>
+									<h2 class="h2-exp">Our first high powered rocket</h2>
+									<div class="read-more read-exp">Read more <img class="arrow" src="assets/icons/arrow.png" alt="arrow"> </div>
+								</a>
+							</div>
+
+
+
+						</div>
+
+					</div>
+
+					<div class="section-row-container full-4">
+
+
+						<section class="grid-1 exp">
+							<div class="item-1">
+								The first UK team in history to participate in the largest rocket engineering competition in the world.
+							</div>
+							<div class="item-2">
+								Award winner of the Best Flight Dynamics at Spaceport America Cup 2018 for achieving an accuracy of 99.83% at 10,000ft.
+							</div>
+							<div class="item-3">
+								SunrIde holds the UK National Altitude Record for rocketry since 2019 at 36,274ft reaching 2.67 Mach according to UKRA.
+							</div>
+							<div class="item-4">
+								In 2023 SunrIde aspires to become the 1st UK University team to reach the Kármán line at 100km! (50 km test in 2021.)
+							</div>
+
+						</section>
+
+
+
+					</div>
+
+
+					<section class="section project-details project-details-case-study">
+
+
+						<section id="idea" class="section">
+							<div class="row">
+								<div class="col-xl-12">
+									<div class="section-row-container">
+
+										<div class="section-row half-padding-bottom">
+
+											<div class="container2">
+
+												<div class="row">
+
+													<div class="col-lg-12">
+														<div class="grid">
+															<div class="text-grid">
+																<h2 class="section-title">
+																	<span data-shadow-text="Who are we?">Who are we?</span>
+																</h2>
+																<p class="lead" style="text-align:justify;">
+																	SunrIde is an ambitious team of 50 hardworking engineering students from Sheffield Hallam University and the University of Sheffield. The team ranges from Undergraduates to Masters students, from more than 20 countries. Founded in 2017, SunrIde has been expanding ever since, maintaining four highly-engineered projects to achieve rocketry innovation through their research.
+																</p>
+															</div>
+															<div class="grid-img">
+																<img src="4.jpg" alt="">
+															</div>
+														</div>
+
+
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section class="section div-color ">
+							<div class="row  div-color ">
+								<div class="col-xl-12 div-color ">
+									<div class="section-row-container div-color ">
+
+										<div class="section-row half-padding-bottom div-color  ">
+
+											<div class="container2 div-color">
+
+												<div class="row">
+
+													<div class="col-lg-12">
+													<h2 class="section-title">
+																	<span data-shadow-text="News">News</span>
+																</h2>
+														<div class="grid-two">
+															<div>
+																<iframe src="https://www.youtube.com/embed/OwXrTvRiIgQ" width="560" height="315" frameBorder="0" autoplay="disabled" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+															</div>
+															<div>
+
+																<iframe width="560" height="315"  src="https://www.youtube.com/embed/Iz9NOh_zJec" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+															</div>
+
+														</div>
+
+
+
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</section>
+						<section class="section">
+							<div class="row">
+								<div class="col-xl-12">
+									<div class="section-row-container">
+
+										<div class="section-row half-padding-bottom ">
+
+											<div class="container2 ">
+
+
+
+
+
+												<!-- <h2 class="section-title">
+															<span data-shadow-text="About Us">About Us</span>
+														</h2> -->
+												<p class="lead" style="text-align:justify;">
+													<img class="img-sponsor" src="Pics/sponsorus.png" alt="">
+												</p>
+
+
+												</a>
+
+
+
+
+
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+				</div>
+			</section>
+
+			<section class="section">
+				<div class="row">
+					<div class="col-xl-12">
+						<div class="section-row-container">
+
+							<div class="top-margin">
+
+								<div class="container2">
+
+									<div class="row">
+
+										<div class="col-lg-12">
+											<div class="sponsor">
+												<a href="sponsors.php">
+													<div>
+														<h2 class="section-title">
+															<span data-shadow-text="Become a sponsor">Become a sponsor</span>
+														</h2>
+													</div>
+
+
+
+												</a>
+
+											</div>
+
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+			<section id="sponsor" class="section" style="background-color:#C6CAD5;">
+
+				<div class="section-row-container div-color">
+
+					<div class="section-row">
+
+						<div class="container2">
+							<h1 class="section-title">
+								<span data-shadow-text="Our Sponsors">Collaborations</span>
+							</h1>
+							<p class="lead"> Thanks to our wonderful sponsors! We extend our sincere appreciation to them.</p>
+						</div>
+						<div class="row">
+
+							<div class="col-xl-12">
+								<div class="sponsor-grid">
+									<div>
+										<div class="sponsor-img">
+											<a href="https://www.sheffield.ac.uk/acse" target="_blank">
+												<img src="demo/clients/1.png" alt="ACSE">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>ACSE Department</h4>
+											<p>Department of Automatic Control and Systems Engineering.</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="https://www.sheffield.ac.uk/faculty/engineering" target="_blank">
+												<img class="img-round" src="demo/clients/3.png" alt="FACULTY OF ENGINEERING">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>Faculty of Engineering</h4>
+											<p>University of Sheffield</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="https://www.ansys.com" target="_blank">
+												<img class="img-round" src="demo/clients/4.png" alt="ANSYS">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>ANSYS, Inc.</h4>
+											<p>ANSYS, Inc. - engineering simulation software</p>
+										</div>
+									</div>
+
+									<div>
+										<div class="sponsor-img">
+											<a href="https://shdcomposites.com/" target="_blank">
+												<img src="demo/clients/5.svg" alt="SHD COMPOSITES" style="width:100%;height:auto;">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>SHD Components</h4>
+											<p>Advanced Composite Materials and Manufacturing</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="http://www.accomposites.co.uk/" target="_blank">
+												<img class="img-round" src="demo/clients/6.png" alt="AC COMPOSITES">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>AC Components</h4>
+											<p>Composite Materials</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="https://www.amrc.co.uk" target="_blank">
+												<img class="img-round" src="demo/clients/7.png" alt="AMRC">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>AMRC</h4>
+											<p>Advanced Manufacturing Research Center</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="http://https://alternum.co.uk/" target="_blank">
+												<img class="img-round" src="demo/clients/8.png" alt="ALTERNUM" style="width:100%;height:auto;">
+											</a>
+										</div>
+
+										<div class="info">
+											<h4>Alternum</h4>
+											<p>Aerospace SME</p>
+										</div>
+									</div>
+									<div>
+										<div class="sponsor-img">
+											<a href="https://www.sheffield.ac.uk/alumni/foundation" target="_blank">
+												<img id="sheff-logo" class="img-round" src="demo/clients/2.png" alt="ALUMNI FOUNDATION">
+											</a>
+										</div>
+
+
+										<div class="info">
+											<h4>Alumni Foundation</h4>
+											<p>The Alumni Foundation, University of Sheffield</p>
+										</div>
+									</div>
+									<div>
+										<a href="http://www.aerosociety.com/">
+											<div class="sponsor-img">
+
+												<img class="img-round  small-img" src="demo/clients/10.png" alt="ALUMNI FOUNDATION">
+
+											</div>
+										</a>
+
+										<div class="info">
+											<h4> Royal Aeronautical Society</h4>
+											<p> Royal Aeronautical Society - RAeS</p>
+										</div>
+									</div>
+									<div>
+										<a href="https://uk.rs-online.com/web/">
+											<div class="sponsor-img">
+
+												<img class="img-round" src="demo/clients/9.png" alt="ALUMNI FOUNDATION">
+
+											</div>
+										</a>
+
+										<div class="info">
+											<h4>RS Components</h4>
+											<p>RS Components Industrial, electronic products & solutions</p>
+										</div>
+									</div>
+									<div>
+										<a href="https://www.sheffield.ac.uk/alumni/foundation" target="_blank">
+											<div class="sponsor-img">
+
+												<img class="img-round small-img" src="demo/clients/11.png" alt="ALUMNI FOUNDATION">
+
+											</div>
+										</a>
+
+										<div class="info">
+											<h4>Sheffield Hallam University</h4>
+											<p>Sheffield Hallam University</p>
+										</div>
+									</div>
+									<div>
+										<a href="http://arundal-astronautics.co.uk/">
+											<div class="sponsor-img">
+
+												<img class="img-round-mid small-img" src="demo/clients/12.png" alt="ALUMNI FOUNDATION">
+
+											</div>
+										</a>
+
+										<div class="info">
+											<h4>Arundal Astronautics</h4>
+											<p>Arundal Astronautics</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+
+			</section>
+
+
+
+
+
+		</div>
+
+		<?php
+
+		include('phpCore/footer.php')
+
+		?>
+
+	</div>
+
+
+	<script type="text/javascript" src="assets/js/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="assets/js/hydrogen.packed.min.js"></script>
+	<!-- <script type="text/javascript" src="assets/js/darkmode.js"></script> -->
+
+	<script type="text/javascript" src="assets/plugins/royalslider/jquery.royalslider.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/mfp/jquery.mfp-0.9.9.js"></script>
+	<script type="text/javascript" src="assets/plugins/mediaelement/mediaelement-and-player.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/gmap/gmap3.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/owlcarousel/owl.carousel.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/isotope/jquery.isotope.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/form/jquery.form.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/form/jquery.validate.min.js"></script>
+
+
+
+</body>
